7dc5f8deda34bd37cd203973812f17599b5bf4c8
Neil Cumpstey <neil.cumpstey@gmail.com>
2021-08-08 09:40:59 +0100

    new model of tuya power strip

--- drivers/cwm/tuya-zigbee-power-strip.src/tuya-zigbee-power-strip.groovy
+++ drivers/cwm/tuya-zigbee-power-strip.src/tuya-zigbee-power-strip.groovy
@@ -2,211 +2,214 @@ import java.lang.Math
 import groovy.json.JsonOutput
 
 metadata {
-    definition(name: "Tuya ZigBee Power Strip", namespace: "cwm", author: "Neil Cumpstey") {
-        capability "Actuator"
-        capability "Refresh"
-        capability "Switch"
+  definition(name: "Tuya ZigBee Power Strip", namespace: "cwm", author: "Neil Cumpstey") {
+    capability "Actuator"
+    capability "Refresh"
 
-        command "childOn", ["string"]
-        command "childOff", ["string"]
+    command "childOn", ["string"]
+    command "childOff", ["string"]
+    command "allOn"
+    command "allOff"
 
-        // Tuya power strip with USB
-        fingerprint endpointId: "01", profileId: "0104", deviceId: "0009", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_vkwryfdr", model: "TS0115", deviceJoinName: "Tuya Zigbee Power Strip"
-    }
+    // Tuya power strip with USB
+    fingerprint endpointId: "01", profileId: "0104", deviceId: "0009", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_vkwryfdr", model: "TS0115", deviceJoinName: "Tuya Zigbee Power Strip"
+    fingerprint endpointId: "01", profileId: "0104", deviceId: "0009", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TZ3000_o005nuxx", model: "TS011F", deviceJoinName: "Tuya Zigbee Power Strip"
+  }
 
-    preferences {
-        input name: "debugLogging", type: "bool", title: "Enable debug logging", defaultValue: true
-    }
+  preferences {
+    input name: "debugLogging", type: "bool", title: "Enable debug logging", defaultValue: true
+  }
 }
 
 def installed() {
-    logger 'trace', "installed"
-    
-    createChildDevices()
-    refresh()
+  logger 'trace', "installed"
+
+  createChildDevices()
+  refresh()
 }
 
 def updated() {
-    logger 'trace', "updated"
+  logger 'trace', "updated"
 
-    createChildDevices()
-    refresh()
+  createChildDevices()
+  refresh()
 }
 
 def parse(String description) {
-    logger 'trace', "parse: ${description}"
-    
-    Map eventMap = zigbee.getEvent(description)
-    Map eventDescMap = zigbee.parseDescriptionAsMap(description)
+  logger 'trace', "parse: ${description}"
 
-    if (!eventMap && eventDescMap) {
-        eventMap = [:]
-        if (eventDescMap?.clusterId == zigbee.ON_OFF_CLUSTER) {
-            eventMap[name] = "switch"
-            eventMap[value] = eventDescMap?.value
-        }
+  Map eventMap = zigbee.getEvent(description)
+  Map eventDescMap = zigbee.parseDescriptionAsMap(description)
+
+  if (!eventMap && eventDescMap) {
+    eventMap = [:]
+    if (eventDescMap?.clusterId == zigbee.ON_OFF_CLUSTER) {
+      eventMap[name] = "switch"
+      eventMap[value] = eventDescMap?.value
     }
+  }
 
-    if (eventMap) {
-        logger 'debug', "eventMap ${eventMap}; eventDescMap ${eventDescMap}"
+  if (eventMap) {
+    logger 'debug', "eventMap ${eventMap}; eventDescMap ${eventDescMap}"
 
-        def endpointId = device.endpointId
-        eventMap[displayed] = true
+    def endpointId = device.endpointId
+    eventMap[displayed] = true
 
-        if (eventDescMap?.sourceEndpoint == endpointId) {
-            logger 'debug', "parse: sendEvent parent ${eventDescMap.sourceEndpoint}"
-            sendEvent(eventMap)
-            // There seem to be duplicate messages sent, one with 'sourceEndpoint'
-            // and one with 'endpoint' to identify the socket being reported, when
-            // it's switched remotely. When it's switched on the device, only the
-            // latter is sent.
-        /*
-        } else if (eventDescMap?.sourceEndpoint) {
-            def childNetworkId = "${device.deviceNetworkId}:${eventDescMap.sourceEndpoint}"
-            def childDevice = childDevices.find {
-                it.deviceNetworkId == childNetworkId
-            }
-            if (childDevice) {
-                log.debug "parse: sendEvent child ${eventDescMap.sourceEndpoint}"
-                childDevice.sendEvent(eventMap)
-            } else {
-                logger 'warn', "Child device: ${childNetworkId} not found"
-            }
-        */
-        } else if (eventDescMap?.endpoint) {
-            def childNetworkId = "${device.deviceNetworkId}:${eventDescMap.endpoint}"
-            def childDevice = childDevices.find {
-                it.deviceNetworkId == childNetworkId
-            }
-            if (childDevice) {
-                log.debug "parse: sendEvent child ${eventDescMap.endpoint}"
-                childDevice.sendEvent(eventMap)
-            } else {
-                logger 'warn', "Child device: ${childNetworkId} not found"
-            } 
-        }
+    // There seem to be duplicate messages sent by the TS0115, one with 'sourceEndpoint'
+    // and one with 'endpoint' to identify the socket being reported, when it's switched
+    // remotely. When it's switched on the device, only the latter is sent.
+    if (eventDescMap?.sourceEndpoint == endpointId) {
+      logger 'debug', "parse: sendEvent parent ${eventDescMap.sourceEndpoint}"
+      sendEvent(eventMap)
+    } else if (eventDescMap?.sourceEndpoint) {
+      def childNetworkId = "${device.deviceNetworkId}:${eventDescMap.sourceEndpoint}"
+      def childDevice = childDevices.find {
+        it.deviceNetworkId == childNetworkId
+      }
+      if (childDevice) {
+        log.debug "parse: sendEvent child ${eventDescMap.sourceEndpoint}"
+        childDevice.sendEvent(eventMap)
+      } else {
+        logger 'warn', "Child device: ${childNetworkId} not found"
+      }
+    } else if (eventDescMap?.endpoint) {
+      def childNetworkId = "${device.deviceNetworkId}:${eventDescMap.endpoint}"
+      def childDevice = childDevices.find {
+        it.deviceNetworkId == childNetworkId
+      }
+      if (childDevice) {
+        logger 'debug', "parse: sendEvent child ${eventDescMap.endpoint}"
+        childDevice.sendEvent(eventMap)
+      } else {
+        logger 'warn', "Child device: ${childNetworkId} not found"
+      } 
     }
+  }
 }
 
 private void createChildDevices() {
-    logger 'trace', "createChildDevices"
+  logger 'trace', "createChildDevices"
 
-    for (i in 1..4) {
-        def endpointHexString = zigbee.convertToHexString(i, 2).toUpperCase()
-        createChildDevice(endpointHexString, "${device.displayName} socket ${i}", "s$i", "Socket $i")
-    }
+  def model = device.getDataValue('model')
+  def sockets
+  switch (model) {
+    case 'TS0115':
+    case 'TS011F':
+      sockets = 4
+      break
+    default:
+      logger 'warn', "Unrecognised model ${model}. Don't know how many child devices to create."
+      return
+  }
+  
+  for (i in 1..sockets) {
+    def endpointHexString = zigbee.convertToHexString(i, 2).toUpperCase()
+    createChildDevice(endpointHexString, "${device.displayName} socket ${i}", "s$i", "Socket $i")
+  }
 
-    createChildDevice('07', "${device.displayName} USB", 'usb', 'USB')
+  switch (model) {
+    case 'TS0115':
+      createChildDevice('07', "${device.displayName} USB", 'usb', 'USB')
+      break
+    case 'TS011F':
+      createChildDevice('05', "${device.displayName} USB", 'usb', 'USB')
+      break
+  }
 }
 
 private void createChildDevice(String endpointHexString, String deviceLabel, String componentName, String componentLabel) {
-    def networkId = "${device.deviceNetworkId}:${endpointHexString}"
-    def childDevice = childDevices.find {
-        it.deviceNetworkId == networkId
-    }
-    if (!childDevice) {
-        logger 'debug', "Creating child device: ${networkId}"
-        addChildDevice('cwm', 'Child Switch', networkId,
-            [completedSetup: true, label: deviceLabel,
-            isComponent: true, componentName: componentName, componentLabel: componentLabel]
-      )
-    } else {
-        logger 'debug', "Child device ${networkId} already exists"
-    }
+  def networkId = "${device.deviceNetworkId}:${endpointHexString}"
+  def childDevice = childDevices.find {
+    it.deviceNetworkId == networkId
+  }
+  if (!childDevice) {
+    logger 'info', "Creating child device: ${networkId}"
+    addChildDevice('cwm', 'Child Switch', networkId,
+                   [completedSetup: true, label: deviceLabel,
+                    isComponent: true, componentName: componentName, componentLabel: componentLabel]
+                  )
+  } else {
+    logger 'debug', "Child device ${networkId} already exists"
+  }
 }
 
 private getChildEndpoint(String networkId) {
-    if (networkId.isNumber()) {
-        return networkId
+  if (networkId.isNumber()) {
+    return networkId
+  }
+
+  networkId.split(":")[-1] as String
+}
+
+def allOn() {
+  logger 'trace', "allOn"
+
+  childDevices.each {
+    if (it.currentValue("switch") != "on") {
+      it.on()
     }
-    
-    networkId.split(":")[-1] as String
+  }
 }
 
-private allChildrenOn() {
-    logger 'trace', "allChildrenOn"
+def allOff() {
+  logger 'trace', "allOff"
 
-    childDevices.each {
-        if (it.currentValue("switch") != "on") {
-            it.on()
-        }
+  childDevices.each {
+    if (it.currentValue("switch") != "off") {
+      it.off()
     }
-}
-
-private allChildrenOff() {
-    logger 'trace', "allChildrenOff"
-
-    childDevices.each {
-        if (it.currentValue("switch") != "off") {
-            it.off()
-        }
-    }
-}
-
-def on() {
-    allChildrenOn()
-}
-
-def off() {
-    allChildrenOff()
+  }
 }
 
 def childOn(String networkId) {
-    logger 'trace', "childOn: ${networkId}"
-    
-    def childEndpoint = getChildEndpoint(networkId)
-    def endpointInt = zigbee.convertHexToInt(childEndpoint)
-    log.debug("child on ${networkId}: ${endpointInt}")
+  logger 'trace', "childOn: ${networkId}"
 
-    "he cmd 0x${device.deviceNetworkId} 0x${childEndpoint} 0x0006 1 { }"
+  def childEndpoint = getChildEndpoint(networkId)
+  def endpointInt = zigbee.convertHexToInt(childEndpoint)
+
+  "he cmd 0x${device.deviceNetworkId} 0x${childEndpoint} 0x0006 1 { }"
 }
 
 def childOff(String networkId) {
-    logger 'trace', "childOff ${networkId}"
-    
-    def childEndpoint = getChildEndpoint(networkId)
-    def endpointInt = zigbee.convertHexToInt(childEndpoint)
+  logger 'trace', "childOff ${networkId}"
 
-    "he cmd 0x${device.deviceNetworkId} 0x${childEndpoint} 0x0006 0 { }"
+  def childEndpoint = getChildEndpoint(networkId)
+  def endpointInt = zigbee.convertHexToInt(childEndpoint)
+
+  "he cmd 0x${device.deviceNetworkId} 0x${childEndpoint} 0x0006 0 { }"
 }
 
 def ping() {
-    return refresh()
+  return refresh()
 }
 
 def refresh() {
-    logger 'trace', 'refresh'
-    
-    def cmds = zigbee.onOffRefresh()
-    childDevices.each {
-        def childEndpoint = getChildEndpoint(it.deviceNetworkId)
-        def endpointInt = zigbee.convertHexToInt(childEndpoint)
-        cmds += zigbee.readAttribute(zigbee.ON_OFF_CLUSTER, 0x0000, [destEndpoint: endpointInt])
-    }
+  logger 'trace', 'refresh'
 
-    return cmds
+  def cmds = zigbee.onOffRefresh()
+  childDevices.each {
+    def childEndpoint = getChildEndpoint(it.deviceNetworkId)
+    def endpointInt = zigbee.convertHexToInt(childEndpoint)
+    cmds += zigbee.readAttribute(zigbee.ON_OFF_CLUSTER, 0x0000, [destEndpoint: endpointInt])
+  }
+
+  return cmds
 }
 
 def poll() {
-    refresh()
+  refresh()
 }
 
 private void logger(String level, message) {
   switch (level) {
     case 'error':
-      log.error message
-      break
     case 'warn':
-      log.warn message
-      break
     case 'info':
-      log.info message
+      log."$level" message
       break
     case 'debug':
-      if (settings.debugLogging) log.debug message
-      break
     case 'trace':
-      if (settings.debugLogging) log.trace message
+      if (settings.debugLogging) log."$level" message
       break
     default:
       log.info message
